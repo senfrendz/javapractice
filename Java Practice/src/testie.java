04eb160b523336128bd6e9570d6effc992d6a1da
senfrendz <senfrendz@gmail.com>
2020-01-06 21:53:17 +0530

    initial commit

---
+++ Java Practice/src/testie.java
@@ -0,0 +1,11 @@
+
+public class testie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Powered by Raju Senthil ");
+		System.out.println("Welcome");
+	}
+
+}
